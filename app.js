a5d8838a370b0818cbc9ebe82a9956506bed1e73
Pooja Chauhan <chauhanpooja3011@gmail.com>
2025-12-07 18:20:28 +0530

    Updated package.json and app.js

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ store.on("error",()=>{
 
 const sessionOptions = {
     store,
-    secret : process.env.SECRET,
+    secret : process.env.SECRET
+    ,
     resave:false,
     saveUninitialized:true,
     cookie:{
